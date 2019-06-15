a76e393364c0e9b781c081d35030093e17d06a78
mrdemqn <mrdemqnvip@gmail.com>
2019-06-15 17:44:55 +0300

    Dz1

---
+++ app/src/main/java/by/itacademy/pvt/dz0/dz0/Dz0MainActivity.kt
@@ -0,0 +1,52 @@
+package by.itacademy.pvt.dz0.dz0
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import by.itacademy.R
+
+
+class Dz0MainActivity : Activity(), View.OnClickListener {
+
+
+    private var helloWorldTextView: TextView? = null
+    private var worldHelloTextView: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_dz0)
+
+        val invertButton = findViewById<Button>(R.id.buttInversion)
+        invertButton.setOnClickListener(this)
+
+        helloWorldTextView = findViewById(R.id.helloWorldTextView)
+        worldHelloTextView = findViewById(R.id.worldHelloTextView)
+
+        helloWorldTextView?.setText(R.string.hello_world)
+
+        helloWorldTextView?.setOnClickListener {inversion()}
+
+        worldHelloTextView?.setOnClickListener{ inversion() }
+    }
+
+    override fun onClick(v: View) {
+        inversion()
+    }
+
+    fun inversion() {
+
+        val helloWorld1TextView = helloWorldTextView?.text.toString()
+        val worldHello2TextView = worldHelloTextView?.text.toString()
+
+        val color1 = helloWorldTextView?.background
+        val color2 = worldHelloTextView?.background
+
+        helloWorldTextView?.background = color2
+        worldHelloTextView?.background = color1
+
+        helloWorldTextView?.text = worldHello2TextView
+        worldHelloTextView?.text = helloWorld1TextView
+    }
+}
