345925dc435ad4fb0b9359524ee6e6f86c55f892
mrdemqn <mrdemqnvip@gmail.com>
2019-06-20 19:22:01 +0300

    no message

--- app/src/main/java/by/itacademy/pvt/dz2/Dz2Activity.kt
+++ app/src/main/java/by/itacademy/pvt/dz2/Dz2Activity.kt
@@ -2,28 +2,37 @@ package by.itacademy.pvt.dz2
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import android.widget.ProgressBar
 import by.itacademy.pvt.R
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import jp.wasabeef.picasso.transformations.CropCircleTransformation
 import kotlinx.android.synthetic.main.activity_dz2.*
+import java.lang.Exception
 
 class Dz2Activity : Activity() {
+    private lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz2)
-        findViewById<View>(R.id.loadImageButton)
+        progressBar = findViewById(R.id.oneProgressBar)
+        findViewById<Button>(R.id.loadImageButton)
             .setOnClickListener {
-                loadImage(oneProgressBar)
+                loadImage()
             }
     }
-    private fun loadImage(progressBar: ProgressBar) {
+    private fun loadImage() {
         progressBar.visibility = ProgressBar.VISIBLE
         Picasso.get()
             .load("https://avatars.mds.yandex.net/get-pdb/964669/92f6c91e-4003-4b9c-abe8-88b5f0b2e96d/orig")
             .transform(CropCircleTransformation())
-            .into(loadImageView)
-        progressBar.visibility = ProgressBar.GONE
+            .into(loadImageView, object: Callback {
+                override fun onSuccess() {
+                    progressBar.visibility = ProgressBar.GONE
+                }
+                override fun onError(e: Exception?) {
+                }
+            })
     }
 }
