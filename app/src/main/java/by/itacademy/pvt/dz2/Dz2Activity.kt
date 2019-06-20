96e41747eb7ee5b7db83dac8a98ca1f9f24cba78
mrdemqn <mrdemqnvip@gmail.com>
2019-06-20 19:23:22 +0300

    Revert "no message"
    This reverts commit 345925dc435ad4fb0b9359524ee6e6f86c55f892.

--- app/src/main/java/by/itacademy/pvt/dz2/Dz2Activity.kt
+++ app/src/main/java/by/itacademy/pvt/dz2/Dz2Activity.kt
@@ -2,37 +2,28 @@ package by.itacademy.pvt.dz2
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
 import android.widget.ProgressBar
 import by.itacademy.pvt.R
-import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import jp.wasabeef.picasso.transformations.CropCircleTransformation
 import kotlinx.android.synthetic.main.activity_dz2.*
-import java.lang.Exception
 
 class Dz2Activity : Activity() {
-    private lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz2)
-        progressBar = findViewById(R.id.oneProgressBar)
-        findViewById<Button>(R.id.loadImageButton)
+        findViewById<View>(R.id.loadImageButton)
             .setOnClickListener {
-                loadImage()
+                loadImage(oneProgressBar)
             }
     }
-    private fun loadImage() {
+    private fun loadImage(progressBar: ProgressBar) {
         progressBar.visibility = ProgressBar.VISIBLE
         Picasso.get()
             .load("https://avatars.mds.yandex.net/get-pdb/964669/92f6c91e-4003-4b9c-abe8-88b5f0b2e96d/orig")
             .transform(CropCircleTransformation())
-            .into(loadImageView, object: Callback {
-                override fun onSuccess() {
-                    progressBar.visibility = ProgressBar.GONE
-                }
-                override fun onError(e: Exception?) {
-                }
-            })
+            .into(loadImageView)
+        progressBar.visibility = ProgressBar.GONE
     }
 }
