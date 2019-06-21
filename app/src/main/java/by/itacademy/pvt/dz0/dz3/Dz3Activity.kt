ff46839560558de3dc65eaa4a56521b0556164b6
mrdemqn <mrdemqnvip@gmail.com>
2019-06-21 14:47:46 +0300

    1

--- app/src/main/java/by/itacademy/pvt/dz0/dz3/Dz3Activity.kt
+++ app/src/main/java/by/itacademy/pvt/dz0/dz3/Dz3Activity.kt
@@ -16,15 +16,8 @@ class Dz3Activity : Activity() {
         setContentView(R.layout.activity_dz3)
         Picasso.get()
             .load("http://www.adm-mosrentgen.ru/wp-content/uploads/2017/07/%D0%BC%D0%B8%D0%BD%D1%8C%D0%BE%D0%BD%D1%8B-%D0%BF%D1%80%D0%B8%D0%BA%D0%BE%D0%BB%D1%8B-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8.jpg")
-            .resize(350,350)
+            .error(R.drawable.error_image)
             .transform(CropCircleTransformation())
-            .into(loadAvatarImageView, object : Callback {
-                override fun onSuccess() {
-                }
-
-                override fun onError(e: Exception?) {
-
-                }
-            })
+            .into(loadAvatarImageView)
     }
 }
