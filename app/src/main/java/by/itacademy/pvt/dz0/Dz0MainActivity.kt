919c473892c1eb8265aff5805c37205decefe800
Кахановский Дмитрий <46688251+mrdemqn@users.noreply.github.com>
2019-06-23 10:38:11 +0300

    Dz2 (#4)
    * Activity Login, URL-image load, ProgressBar
    
    * С помощью библиотеки Picasso сделал так, чтобы картинка отображалась круглой.
    
    * Dz2Activity
    
    Изменил ProgressBar.
    
    * no message
    
    * Revert "no message"
    
    This reverts commit 345925dc435ad4fb0b9359524ee6e6f86c55f892.
    
    * изменил ProgressBar
    
    * Progress
    
    * Проверка на ошибку.

--- app/src/main/java/by/itacademy/pvt/dz0/Dz0MainActivity.kt
+++ app/src/main/java/by/itacademy/pvt/dz0/Dz0MainActivity.kt
@@ -5,12 +5,11 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import by.itacademy.R
-
+import by.itacademy.pvt.R
 
 class Dz0MainActivity : Activity(), View.OnClickListener {
 
-private var helloWorldTextView: TextView? = null
+    private var helloWorldTextView: TextView? = null
     private var worldHelloTextView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,16 +24,16 @@ private var helloWorldTextView: TextView? = null
 
         helloWorldTextView?.setText(R.string.hello_world)
 
-        helloWorldTextView?.setOnClickListener {inversion()}
+        helloWorldTextView?.setOnClickListener { inversion() }
 
-        worldHelloTextView?.setOnClickListener{ inversion() }
+        worldHelloTextView?.setOnClickListener { inversion() }
     }
 
     override fun onClick(v: View) {
         inversion()
     }
 
-    fun inversion() {
+    private fun inversion() {
 
         val helloWorld1TextView = helloWorldTextView?.text.toString()
         val worldHello2TextView = worldHelloTextView?.text.toString()
@@ -47,5 +46,5 @@ private var helloWorldTextView: TextView? = null
 
         helloWorldTextView?.text = worldHello2TextView
         worldHelloTextView?.text = helloWorld1TextView
-    }
+        }
 }
