0fd15eb6c577398362fae9bf181297f16f907861
mrdemqn <mrdemqnvip@gmail.com>
2019-06-27 17:27:53 +0300

    dz5

--- app/src/main/java/by/itacademy/pvt/dz5/Dz5Activity.kt
+++ app/src/main/java/by/itacademy/pvt/dz5/Dz5Activity.kt
@@ -8,5 +8,8 @@ class Dz5Activity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz5)
+
+        val pieChartView = findViewById<Dz5View>(R.id.dz5View)
+        pieChartView.pieChartValues = floatArrayOf(30f, 20f, 50f, 60f)
     }
 }
--- app/src/main/java/by/itacademy/pvt/dz5/Dz5View.kt
+++ app/src/main/java/by/itacademy/pvt/dz5/Dz5View.kt
@@ -1,22 +1,78 @@
 package by.itacademy.pvt.dz5
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Build
 import android.support.annotation.RequiresApi
 import android.util.AttributeSet
 import android.view.View
+import android.graphics.RectF
+import android.support.v4.content.ContextCompat
+import by.itacademy.pvt.R
+
 
 class Dz5View : View {
+
+    private val rectF = RectF()
+    private val slicePaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val paintColors = arrayOf(Color.BLUE, Color.GREEN, Color.MAGENTA, Color.RED)
+
+    private var scaledValues = FloatArray(0)
+    var pieChartValues: FloatArray = FloatArray(0)
+        set(value) {
+            field = value
+            scaledValues = scale(value)
+            invalidate()
+        }
 
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(
-        context,
-        attrs,
-        defStyleAttr,
-        defStyleRes
-    )
+    init {
+        slicePaint.style = Paint.Style.FILL
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas ?: return
+
+        var sliceStartPoint = 0f
+        for (i in 0 until pieChartValues.size step 1) {
+            slicePaint.color = paintColors[i]
+            canvas.drawArc(rectF, sliceStartPoint, scaledValues[i], true, slicePaint)
+            sliceStartPoint += scaledValues[i]
+        }
+    }
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+        val padding = Math.min(width, height) * 0.10f
+
+        rectF.left = padding
+        rectF.top = padding
+        rectF.right = Math.min(width, height).toFloat() - padding
+        rectF.bottom = Math.min(width, height).toFloat() - padding
+    }
+
+    private fun scale(array: FloatArray): FloatArray {
+        var scaledValues = FloatArray(array.size)
+        val sum = getSum(array)
+        for (i in 0 until array.size step 1) {
+            scaledValues[i] = array[i] / sum * 360f
+        }
+        return scaledValues
+    }
+
+    private fun getSum(array: FloatArray): Float {
+        var sum = 0f
+        for (i in array) {
+            sum += i
+        }
+        return sum
+    }
 }
